dd5841aac8140ab04c8c621deb82bd8cff741b1b
Martin Andreev <martin_i_a@abv.bg>
2024-09-28 17:29:58 +0300

    Build reusable styled components for rows and refactor the styled component for buttons
    Now they are styled more compactly.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
+import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.main`
+  padding: 20px;
+`;
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Heading as="h1">Azure Oazis</Heading>
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Azure Oazis</Heading>
 
-      <Heading as="h2">Check in and out</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button
+                // variation="primary"
+                // size="medium"
+                onClick={() => alert("Check in")}
+              >
+                Check in
+              </Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Check in")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
 
-      <Heading as="h3">Form</Heading>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
     </>
   );
 }
