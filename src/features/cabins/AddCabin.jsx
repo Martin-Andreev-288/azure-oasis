d86dc6d1b254a605dbfe4e365feffcef5ea22dbe
Martin Andreev <martin_i_a@abv.bg>
2024-10-04 00:46:21 +0300

    Setup modal windows for editing and deleting
    Enable confirm message for the deleting.

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,26 +1,19 @@
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
-
+// reuse the modal window for editing and deleting
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
-
+    <div>
       <Modal>
-        <Modal.Open opens="table">
-          <Button>Show table</Button>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
         </Modal.Open>
-        <Modal.Window name="table">
+        <Modal.Window name="cabin-form">
           <CreateCabinForm />
         </Modal.Window>
       </Modal>
-    </Modal>
+    </div>
   );
 }
 
